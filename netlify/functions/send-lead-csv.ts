90c60e8baf6cc46ac09892f7c885b61b8fbbbf3e
Matthew LeFevre <matthewlefevre95@gmail.com>
2024-08-23 09:57:44 -0600

    👌 IMPROVE: Removing old work email from the recieving emails.

--- netlify/functions/send-lead-csv.ts
+++ netlify/functions/send-lead-csv.ts
@@ -19,11 +19,7 @@ const handler: Handler = async () => {
     const data = await readFile("/tmp/leads.csv");
     email.send({
       // reciever: ["scall@portersop.com"],
-      reciever: [
-        "mlefevre@everlastbrands.com",
-        "matthewlefevre95@gmail.com",
-        "scall@portersop.com",
-      ],
+      reciever: ["matthewlefevre95@gmail.com", "scall@portersop.com"],
       subject: "[New Leads] Porter Lead Gen Tool",
       attachment: {
         data: data,
